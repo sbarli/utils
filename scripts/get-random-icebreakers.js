569f456304f9f4f4a07e2e7051430577f3b7fc97
Samantha Salley <samanthasalley@gmail.com>
2022-01-13 16:52:41 -0500

    cleanup icebreakers code

--- scripts/get-random-icebreakers.js
+++ scripts/get-random-icebreakers.js
@@ -3,6 +3,7 @@ const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 const icebreakers = require('../data/icebreaker-questions');
 const randomListGroups = require('../functions/random-list-groups');
 const { getRandomIntegerInclusive } = require('../functions/get-random-integer');
+const { getIcebreakers } = require('../functions/get-icebreakers');
 
 const convertStringBooleanToBoolean = (initVal, exact = true) => {
   if (initVal === 'true') {
@@ -29,21 +30,32 @@ const ALLOW_LESS_THAN = parsedArgs.al ? parsedArgs.al : false;
 const ALLOW_MORE_THAN = parsedArgs.am ? parsedArgs.am : false;
 const SELECT_ONE = parsedArgs.select ? parsedArgs.select : false;
 
-console.log('WILL HAVE LEFTOVERS: ', icebreakers.length % QUESTIONS_PER_GROUP !== 0);
+const selectedIcebreakers = getIcebreakers({ 
+  questionsPerGroup: QUESTIONS_PER_GROUP, 
+  allowLessThan: ALLOW_LESS_THAN, 
+  allowMoreThan: ALLOW_MORE_THAN, 
+  selectOne: SELECT_ONE,
+})
 
-const icebreakerGroups = randomListGroups(icebreakers, QUESTIONS_PER_GROUP, ALLOW_LESS_THAN, ALLOW_MORE_THAN);
+console.log(`
+  QUESTIONS:
+    ${JSON.stringify(selectedIcebreakers, null, 2)}
+`);
 
-if (SELECT_ONE) {
-  const randomlySelectedGroup = getRandomIntegerInclusive(0, icebreakerGroups.length - 1);
-  const randomIcebreakers = icebreakerGroups[randomlySelectedGroup];
-  console.log(`
-    QUESTIONS:
-      ${JSON.stringify(randomIcebreakers, null, 2)}
-  `);
-}
-else {
-  console.log(`
-    QUESTIONS:
-      ${JSON.stringify(icebreakerGroups, null, 2)}
-  `);
-}
+// const icebreakerGroups = randomListGroups(icebreakers, QUESTIONS_PER_GROUP, ALLOW_LESS_THAN, ALLOW_MORE_THAN);
+
+
+// if (SELECT_ONE) {
+//   const randomlySelectedGroup = getRandomIntegerInclusive(0, icebreakerGroups.length - 1);
+//   const randomIcebreakers = icebreakerGroups[randomlySelectedGroup];
+//   console.log(`
+//     QUESTIONS:
+//       ${JSON.stringify(randomIcebreakers, null, 2)}
+//   `);
+// }
+// else {
+//   console.log(`
+//     QUESTIONS:
+//       ${JSON.stringify(icebreakerGroups, null, 2)}
+//   `);
+// }
